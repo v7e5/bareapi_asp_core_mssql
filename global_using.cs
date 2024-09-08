265823845c1e20f426ab477912aa5667fed68ae8
v7e5 <jv0@tuta.io>
2024-09-08 15:12:10 +0530

    init

---
+++ global_using.cs
@@ -0,0 +1,20 @@
+global using System;
+global using System.Data;
+global using System.Linq;
+global using System.Text;
+global using System.Text.Json;
+global using System.Threading.Tasks;
+global using System.Collections.Frozen;
+global using System.Collections.Generic;
+global using System.Security.Cryptography;
+
+global using Microsoft.Data.SqlClient;
+
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using static util.Util;
---
+++ util.cs
@@ -0,0 +1,174 @@
+﻿namespace util;
+
+static class Util {
+
+  public static void cl<T>(T x) {
+    Console.WriteLine(x);
+  }
+
+  public static void z<T>(this T t) {
+    var ct = t?.GetType();
+    var rt = typeof(T);
+    cl(ct?.GetType());
+    cl(rt.GetType());
+    cl(rt is System.Type);
+    cl(ct is System.Type);
+    cl($"{ct} : {rt}");
+  }
+
+  public static string? _str(this JsonElement j, string k) =>
+    (j.ValueKind is JsonValueKind.Object
+      && j.TryGetProperty(k, out var v)
+      && v.ValueKind is JsonValueKind.String
+      && v.GetString()?.Trim() is string s
+      && s.Length != 0
+    ) ? s : null;
+
+  public static long? _long(this JsonElement j, string k) =>
+    (j.ValueKind is JsonValueKind.Object
+      && j.TryGetProperty(k, out var v)
+      && v.ValueKind is JsonValueKind.Number
+      && v.TryGetInt64(out var i)
+    ) ? i : null;
+
+  public static bool? _bool(this JsonElement j, string k) =>
+    (j.ValueKind is JsonValueKind.Object
+      && j.TryGetProperty(k, out var v)
+      && v.ValueKind is (JsonValueKind.True or JsonValueKind.False)
+    ) ? v.GetBoolean() : null;
+
+  public static JsonElement.ArrayEnumerator? _arr(
+    this JsonElement j, string k
+  ) =>
+    (j.ValueKind is JsonValueKind.Object
+      && j.TryGetProperty(k, out var v)
+      && v.ValueKind is JsonValueKind.Array
+    ) ? v.EnumerateArray() : null;
+
+  public static IEnumerable<T> _reverse<T>(
+    this IEnumerable<T> source, bool reverse = true
+  ) => reverse ? source.Reverse() : source;
+
+  public static IDictionary<string, object>[] ToDictArray(
+    this SqlDataReader reader, bool reverse = false
+  ) => reader
+    .Cast<IDataRecord>()
+    .Select(e => Enumerable.Range(0, e.FieldCount)
+      .ToDictionary(e.GetName, e.GetValue))
+    ._reverse(reverse)
+    .ToArray();
+
+  public static byte[] deriveKey(string password, byte[] salt) =>
+    Rfc2898DeriveBytes.Pbkdf2(
+      password: password,
+      salt: salt,
+      iterations: 100000,
+      hashAlgorithm: HashAlgorithmName.SHA512,
+      outputLength: 32
+    );
+
+  public static FrozenDictionary<string, JsonElement> fd(this JsonElement o) =>
+    o.EnumerateObject().ToFrozenDictionary(kv => kv.Name, kv => kv.Value);
+
+  public static FrozenDictionary<String, String> env() =>
+    Environment
+      .GetEnvironmentVariables()
+      .Cast<System.Collections.DictionaryEntry>()
+      .ToFrozenDictionary(
+        kv => kv.Key.ToString() ?? "",
+        kv => kv.Value?.ToString() ?? ""
+      );
+
+  public static Task<string> env_str() => Task.Run(() => dump(env()));
+
+  public static string dump(FrozenDictionary<string, string> x) {
+    var c1 = x.Keys.Max(k => k.Length);
+    var c2 = x.Values.Max(v => v.Length);
+
+    var t_c1 = new String('━', c1);
+    var t_c2 = new String('━', c2);
+    var s_c2 = new String('─', c2);
+
+    var t = '┏' + t_c1 + '┳' + t_c2 + '┑' + Environment.NewLine + '┃';
+    var b = '│' + Environment.NewLine + '┗' + t_c1 + '┻' + t_c2 + '┙';
+    var j = '│' + Environment.NewLine
+      + '┣' + t_c1 + '┫' + s_c2 + '┤' + Environment.NewLine + '┃';
+
+    return new StringBuilder(t)
+      .AppendJoin(j, x.Select(
+        kv => kv.Key.PadRight(c1) + '┃' + kv.Value.PadRight(c2)))
+      .Append(b).ToString();
+  }
+
+  public static int[] collatz(int[] a) => a[^1] switch {
+    1 => a, 
+    int n => collatz([..a, n % 2 == 0 ? n/2 : (3*n)+1])
+  };
+
+}
+
+sealed class NonGen: System.Collections.IEnumerable {
+  int[] data = {1, 2, 3};
+
+  public System.Collections.IEnumerator GetEnumerator() => new Rator(this);
+
+  class Rator: System.Collections.IEnumerator, IDisposable {
+    NonGen collection;
+    int currentIndex = -1;
+
+    public Rator (NonGen items) => this.collection = items;
+
+    public object Current => currentIndex switch {
+      _ when currentIndex == -1 =>
+        throw new InvalidOperationException ("Enumeration not started!"),
+      _ when currentIndex == collection.data.Length =>
+        throw new InvalidOperationException ("Past end of list!"),
+      _ => collection.data[currentIndex]
+    };
+
+    public bool MoveNext() {
+      cl($"nongen move {currentIndex}");
+      return (currentIndex >= collection.data.Length - 1)
+        ? false : ++currentIndex < collection.data.Length;
+    }
+
+    public void Reset() => currentIndex = -1;
+
+    void IDisposable.Dispose() {
+      cl("nongen dispose"); 
+    }
+  }
+}
+
+sealed class ConGen: IEnumerable<int> {
+  int[] data = {3, 2, 1};
+
+  public IEnumerator<int> GetEnumerator() => new Rator(this);
+
+  System.Collections.IEnumerator
+    System.Collections.IEnumerable.GetEnumerator() =>
+      throw new Exception("non gen n/a");
+
+  class Rator: IEnumerator<int> {
+    ConGen collection;
+    int currentIndex = -1;
+
+    public Rator (ConGen items) => this.collection = items;
+
+    public bool MoveNext() {
+      cl($"congen move {currentIndex}");
+      return ++currentIndex < collection.data.Length;
+    }
+
+    public int Current => collection.data [currentIndex];
+
+    public void Reset() => currentIndex = -1;
+
+    void IDisposable.Dispose() {
+      cl("congen dispose"); 
+    }
+
+    object System.Collections.IEnumerator.Current =>
+      throw new Exception("non gen n/a");
+  }
+}
